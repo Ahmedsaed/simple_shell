fe27a9e5208dff960070ec6b177ffbb53b29df2b
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-04-24 01:48:49 +0200

    update integration tests to handle stderr

--- tests/integration/one_cmd_no_args.py
+++ tests/integration/one_cmd_no_args.py
@@ -7,21 +7,29 @@ shell_input = "\n".join([
         "ls",
     ]) 
 
-# Run your compiled shell executable and capture its output
-shell_output = subprocess.run("./build/shell.out", input=shell_input, capture_output=True, text=True).stdout
-shell_output = shell_output.replace("$ ", "").replace("\nexit", "").strip();
+# run your compiled shell executable and capture its output
+shell_process = subprocess.run("./build/shell.out", input=shell_input, capture_output=True, text=True)
+shell_output = shell_process.stdout.replace("$ ", "").strip()
+shell_stderr = shell_process.stderr
 
-# Run bash and capture its output
-bash_output = subprocess.run("/bin/sh", input=shell_input, capture_output=True, text=True).stdout.strip();
+# run bash and capture its output
+bash_process = subprocess.run("/bin/sh", input=shell_input, capture_output=True, text=True)
+bash_output = bash_process.stdout.strip()
+bash_stderr = bash_process.stderr
 
-# Compare the output of your shell and bash
+# compare the output of your shell and bash
 sm = difflib.SequenceMatcher(None, shell_output, bash_output)
-if sm.ratio() == 1:
-    print(f"Integration test {os.path.basename(__file__)} passed!")
+if sm.ratio() == 1 and len(shell_stderr.split('\n')) == len(bash_stderr.split('\n')):
+    print(f"integration test {os.path.basename(__file__)} passed!")
 else:
-    print(f"Integration test {os.path.basename(__file__)} failed.")
-    print("Shell output: ", shell_output)
-    print("Bash output: ", bash_output)
-    dif = difflib.Differ()
-    shell_diff = dif.compare(shell_output.splitlines(), bash_output.splitlines())
-    print("\n".join(shell_diff))
+    print(f"integration test {os.path.basename(__file__)} failed.")
+    if len(shell_stderr.split('\n')) != len(bash_stderr.split('\n')):
+        print(f"stderr is not similar")
+        print("shell error: ", shell_stderr)
+        print("bash error: ", bash_stderr)
+    else:
+        print("shell output: ", shell_output)
+        print("bash output: ", bash_output)
+        dif = difflib.differ()
+        shell_diff = dif.compare(shell_output.splitlines(), bash_output.splitlines())
+        print("\n".join(shell_diff))
