175ab47dd6db97a801a08f3ef2f4b533594f7de4
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-04-24 01:48:49 +0200

    update integration tests to handle stderr

--- tests/integration/invalid_commands.py
+++ tests/integration/invalid_commands.py
@@ -8,26 +8,29 @@ shell_input = "\n".join([
         "asdddecho \"hallo world\"",
     ])
 
-# Run your compiled shell executable and capture its output
+# run your compiled shell executable and capture its output
 shell_process = subprocess.run("./build/shell.out", input=shell_input, capture_output=True, text=True)
 shell_output = shell_process.stdout.replace("$ ", "").strip()
 shell_stderr = shell_process.stderr
 
-# Run bash and capture its output
+# run bash and capture its output
 bash_process = subprocess.run("/bin/sh", input=shell_input, capture_output=True, text=True)
 bash_output = bash_process.stdout.strip()
 bash_stderr = bash_process.stderr
 
-# Compare the output of your shell and bash
+# compare the output of your shell and bash
 sm = difflib.SequenceMatcher(None, shell_output, bash_output)
 if sm.ratio() == 1 and len(shell_stderr.split('\n')) == len(bash_stderr.split('\n')):
-    print(f"Integration test {os.path.basename(__file__)} passed!")
+    print(f"integration test {os.path.basename(__file__)} passed!")
 else:
+    print(f"integration test {os.path.basename(__file__)} failed.")
     if len(shell_stderr.split('\n')) != len(bash_stderr.split('\n')):
-        print(f"STDERR is not similar", shell_stderr, bash_stderr)
-    print(f"Integration test {os.path.basename(__file__)} failed.")
-    print("Shell output: ", shell_output)
-    print("Bash output: ", bash_output)
-    dif = difflib.Differ()
-    shell_diff = dif.compare(shell_output.splitlines(), bash_output.splitlines())
-    print("\n".join(shell_diff))
+        print(f"stderr is not similar")
+        print("shell error: ", shell_stderr)
+        print("bash error: ", bash_stderr)
+    else:
+        print("shell output: ", shell_output)
+        print("bash output: ", bash_output)
+        dif = difflib.Differ()
+        shell_diff = dif.compare(shell_output.splitlines(), bash_output.splitlines())
+        print("\n".join(shell_diff))
